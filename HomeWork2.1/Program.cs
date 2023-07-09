c174497dbc653dd792542c2b43623051eb426915
Malik <poseydon100500@gmail.com>
2023-07-09 14:53:51 +0500

    Добавьте файлы проекта.

---
+++ HomeWork2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите целое число");
+        int number = int.Parse(Console.ReadLine());
+        int cube = number * number * number;
+        Console.WriteLine("Куб введеного числа" + cube);
+        Console.ReadKey();
+    }
+}
